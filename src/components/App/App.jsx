2e9426f5940dcf75e9abea1a1227d61771c1366c
SOFIA <sofia.nesimko@gmail.com>
2024-06-11 09:20:57 +0200

    redux add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,50 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from '../../redux/store';
 import ContactForm from '../ContactForm/ContactForm'
 import ContactList from '../ContactList/ContactList';
 import SearchBox from '../SearchBox/SearchBox';
 import './App.module.css';
 
-const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    return storedContacts ? JSON.parse(storedContacts) : [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
-  });
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = (newContact) => {
-    setContacts([...contacts, newContact]);
-  };
-
-  const deleteContact = (id) => {
-    setContacts(contacts.filter((contact) => contact.id !== id));
-  };
-
-  const [filter, setFilter] = useState('');
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm addContact={addContact} />
-      <SearchBox filter={filter} onFilterChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
-    </div>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <div id="root">
+        <h1>Contact Book</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
