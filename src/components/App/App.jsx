ece7691ec73de2a820fec0c67af2028442f3b876
SOFIA <sofia.nesimko@gmail.com>
2024-06-12 11:54:14 +0200

    deployment test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from '../../redux/store';
 import ContactForm from '../ContactForm/ContactForm'
 import ContactList from '../ContactList/ContactList';
 import SearchBox from '../SearchBox/SearchBox';
+import { selectContacts } from '../../redux/contactsSlice';
 import './App.module.css';
 
 
 const App = () => (
-  <div>
-    <h1>Contact Book</h1>
-    <ContactForm />
-    <SearchBox />
-    <ContactList />
-  </div>
+      <div>
+        <h1>Contact Book</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
 );
 
 export default App;
